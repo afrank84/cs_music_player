aeabc65bbba488a441c3fa9cf4024ceeed704dd0
Anthony Frank <afrank84@yahoo.com>
2024-01-05 17:55:17 -0500

    Added a file open menu at top of the program, also all mp3 files are now listed.

--- musicApp3/MainWindow.xaml.cs
+++ musicApp3/MainWindow.xaml.cs
@@ -1,28 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using Microsoft.Win32;
 
 namespace musicApp3
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<MP3FileInfo> MP3Files { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            MP3Files = new ObservableCollection<MP3FileInfo>();
+            mp3ListView.ItemsSource = MP3Files;
+        }
+
+        private void OpenFolder_Click(object sender, RoutedEventArgs e)
+        {
+            var folderDialog = new OpenFileDialog
+            {
+                Title = "Select a Folder",
+                CheckFileExists = false,
+                CheckPathExists = true,
+                FileName = "Select Folder",
+                Filter = "Folder|*.thisisfake"
+            };
+
+            if (folderDialog.ShowDialog() == true)
+            {
+                string selectedFolderPath = System.IO.Path.GetDirectoryName(folderDialog.FileName);
+                string[] mp3Files = Directory.GetFiles(selectedFolderPath, "*.mp3");
+
+                MP3Files.Clear();
+
+                foreach (string mp3File in mp3Files)
+                {
+                    MP3Files.Add(new MP3FileInfo
+                    {
+                        FileName = Path.GetFileName(mp3File),
+                        FilePath = mp3File
+                    });
+                }
+            }
+        }
+
+        public class MP3FileInfo
+        {
+            public string FileName { get; set; }
+            public string FilePath { get; set; }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
