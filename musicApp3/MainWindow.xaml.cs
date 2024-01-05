72d9da96a5e4b9d87a9b3dd4244c0ac5d648436f
Anthony Frank <afrank84@yahoo.com>
2024-01-05 18:36:03 -0500

    Added tagsharper meat data nuget

--- musicApp3/MainWindow.xaml.cs
+++ musicApp3/MainWindow.xaml.cs
@@ -3,6 +3,9 @@ using System.IO;
 using System.Windows;
 using System.Collections.ObjectModel;
 using Microsoft.Win32;
+using System.Diagnostics;
+using TagLib;
+
 
 namespace musicApp3
 {
@@ -24,8 +27,8 @@ namespace musicApp3
                 Title = "Select a Folder",
                 CheckFileExists = false,
                 CheckPathExists = true,
-                FileName = "Select Folder",
-                Filter = "Folder|*.thisisfake"
+                FileName = Environment.CurrentDirectory,
+                Filter = "Folder|*.folder"
             };
 
             if (folderDialog.ShowDialog() == true)
@@ -37,19 +40,47 @@ namespace musicApp3
 
                 foreach (string mp3File in mp3Files)
                 {
-                    MP3Files.Add(new MP3FileInfo
+                    var mp3Info = new MP3FileInfo
                     {
                         FileName = Path.GetFileName(mp3File),
                         FilePath = mp3File
-                    });
+                    };
+
+                    try
+                    {
+                        using (var tagFile = TagLib.File.Create(mp3File))
+                        {
+                            mp3Info.FileSizeBytes = new FileInfo(mp3File).Length; // Get file size in bytes
+                            mp3Info.Duration = tagFile.Properties.Duration.ToString(@"mm\:ss");
+                            mp3Info.Artist = tagFile.Tag.FirstPerformer;
+                            mp3Info.Album = tagFile.Tag.Album;
+                            mp3Info.Year = (int)tagFile.Tag.Year;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        // Handle exceptions if there is an issue reading the MP3 file's metadata
+                        mp3Info.Duration = "N/A";
+                        mp3Info.Artist = "N/A";
+                        mp3Info.Album = "N/A";
+                        mp3Info.Year = 0;
+                    }
+
+                    MP3Files.Add(mp3Info);
                 }
             }
+
         }
 
         public class MP3FileInfo
         {
             public string FileName { get; set; }
             public string FilePath { get; set; }
+            public long FileSizeBytes { get; set; }
+            public string Duration { get; set; }
+            public string Artist { get; set; }
+            public string Album { get; set; }
+            public int Year { get; set; }
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
